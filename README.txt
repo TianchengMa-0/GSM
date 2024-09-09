c1ca47e42b9a181d38b50a0a71ab9218e4b212a2
Tiancheng-NLC <tima@ucsd.edu>
2024-09-09 07:18:47 -0500

    1.0.2

--- README.txt
+++ README.txt
@@ -5,3 +5,10 @@ npm install dotenv
 npm install -g nodemon
 
 install mysql 
+install dropbox
+
+install ngrok for temp localhost expose to public for testing dropbox webhook, place exe file to environment path
+ngrok config add-authtoken YOUR_AUTH_TOKEN
+start backend with port
+ngrok http PORT_NUMBER
+
--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,46 @@
-var mysql = require('mysql');
+// init app
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+
+// CONNECT TO DB
+const mysql = require('mysql');
 require('dotenv').config();
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 con.connect(function (err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected to MySQL Database!");
+});
+
+
+// SET WEBHOOK
+app.post('/webhook/dropbox', (req, res) => {
+
+    const changes = req.body;
+    console.log('Dropbox webhook received:', changes);
+    res.sendStatus(200);
+
+});
+app.get('/webhook/dropbox', (req, res) => {
+    if (req.query.challenge) {
+        res.send(req.query.challenge);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+
+// start app
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
